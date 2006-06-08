7cdfe067cf0f9fa0b9e7d3a9d36af7ed7e0cbef7
DWKnight <DWKnight@9886c8e6-f642-3646-a7d1-a5e7bcdcff54>
2006-06-08 21:00:53 +0000

    Nonfunctional build, segmentation fault on piece data type conversion.

--- TorrentBuild_CPPMakeFunctions.cpp
+++ TorrentBuild_CPPMakeFunctions.cpp
@@ -1,3 +1,5 @@
+
+#include "SHA1.h"
 #include "atom.h"
 #include "bencode.h"
 #include "util.h"
@@ -5,8 +7,10 @@
 
 #include "TorrentBuild_CPPFunctions.h"
 #include "TorrentBuild_CPPDlg.h"
-#include "SHA1.h"
-#include "md5.hh"
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
 
 void TorrentBuild_CPPDlg::MakeTorrentFromFile( string fileToMake , int pieceSize, string announceurl, CAtom *announcelist,
 string comment, bool privateTorrent, bool gensha1, bool gencrc32,
@@ -57,6 +61,32 @@ bool genmd5, bool gened2k, bool gentth, bool genexternals)
          OptionalHashProgress->SetValue( OptionalHashes );
      }
 */
+
+     // Begin File Piece Hashing
+   	 ifstream filetoopen;
+ 	 filetoopen.open( fileToMake.c_str() );
+ 	 string pieces;
+     char * memblock;
+   	 CSHA1 *piecesha1 = new CSHA1();
+   	 int piececount;
+ 	 while ( filetoopen.eof() == false )
+     {
+         piecesha1 = new CSHA1();
+         memblock = new char[pieceSize];
+    	 filetoopen.read( memblock, pieceSize );
+     	 piecesha1->Update((UINT_8 *)memblock, pieceSize);
+     	 piecesha1->Final();
+         char *pieceobtained = new char[40];
+     	 piecesha1->ReportHash(pieceobtained);
+     	 // segfault
+     	 pieces = pieces + UTIL_HashToString ( string(pieceobtained) );
+     	 piececount++;
+     	 filetoopen.seekg(piececount*pieceSize, ios::beg);
+     	 delete memblock;
+     	 delete piecesha1;
+     	 delete pieceobtained;
+     }
+     pTorrentInfo->setItem("pieces", new CAtomString( pieces ) );
      string pathForTorrent = fileToMake + string(".torrent");
      UTIL_MakeFile( pathForTorrent.c_str() , Encode( pTorrentRoot ) );
 }
